7b74cc461ec4bd530ed53b5e3afd70fcd40cfd3e
Giacomo <giacomo.clerico@outlook.it>
2025-07-17 21:39:46 +0200

    feat: remove possibility of new user, hardcode admin user via seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+user = User.where(email: "giacomo.clerico@outlook.it").first_or_initialize
+user.update!(
+    password: "SuperSecretPassword",
+    password_confirmation: "SuperSecretPassword"
+)
